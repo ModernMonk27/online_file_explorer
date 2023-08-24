db110a255e83f33ca780074a951a51ef0478cc38
monk <lakshmiaravind.atom@gmail.com>
2023-08-24 09:03:00 +0530

    added filepath

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-// import packages
+//You will build this project using pure Node to gain a thorough understanding of Node. There are obviously easier ways of building this project using external modules, but that is the purpose of this project as you would miss out on many code concepts.
 
+
+//require node modules
 const http = require('http');
 
+//file imports
 const respond = require('./lib/respond');
 
+//connection settings
+const port = process.env.PORT || 3000;
 
-// create server 
+//Create server
+const server = http.createServer(respond);
 
-const server = http.createServer(respond)
-
-// connection settings
-
-const port = process.env.port || 3000 ;
-
-
-// listen to client 
-
+//listen to client requests on the specific port, the port should be available
 server.listen(port, () => {
-    console.log(`listening to the port : ${port}`);
-})
+    console.log(`listening on port: ${port}`);
+});
